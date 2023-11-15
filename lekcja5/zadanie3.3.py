0db4140538c377b9042f77e682f16cc26b3f0012
Szymon Sulejczak <contact@szymonsulejczak.com>
2023-11-15 12:51:22 +0100

    add lekcja6

--- lekcja5/zadanie3.3.py
+++ lekcja5/zadanie3.3.py
@@ -1,5 +1,5 @@
 # otwieramy plik w trybie do odczytu
-plik = open('pi.txt', 'r')
+plik = open('pi_przyklad.txt', 'r')
 # wyciągamy wszystkie wiersze z pliku
 wiersze = plik.readlines()
 
@@ -9,29 +9,37 @@ licznik = 0
 # Lista ciagi przechowuje znalezione ciągi
 ciagi = []
 
+ciagTest = []
+
 # Przeglądamy wszystkie możliwe ciągi o długości 6
 for i in range(0, len(wiersze) - 5):
 
     # Czy aktualny ciąg jest rosnący?
     czyRosnacy = True
-
     # Czy aktualny ciąg jest malejący?
     czyMalejacy = True
 
     # Przechowuje poprzednią liczbę w ciągu
     poprzedniaLiczba = int(wiersze[i])
 
+    print(ciagTest)
+    ciagTest = []
+
     # Przeglądamy kolejne liczby w ciągu
     for j in range(1, 6):
+
 
         # Aktualna liczba w ciągu
         obecnaLiczba = int(wiersze[i + j])
 
-        # Jeśli aktualny ciąg jest rosnący i obecna liczba jest mniejsza lub równa poprzedniej, to ciąg nie jest rosnący
-        if czyRosnacy and obecnaLiczba <= poprzedniaLiczba:
+        ciagTest.append(obecnaLiczba)
 
+        # Jeśli aktualny ciąg jest rosnący i maleje lub liczba obecna jest taka sama jak poprzednia
+        if czyRosnacy and obecnaLiczba <= poprzedniaLiczba:
             # Jeśli ciąg ma długość 1, to nie może być ani rosnący, ani malejący
             if i + 1 == i + j:
+                print(obecnaLiczba)
+                print(poprzedniaLiczba)
                 czyRosnacy = False
                 czyMalejacy = False
                 break
@@ -39,7 +47,7 @@ for i in range(0, len(wiersze) - 5):
             # Ciąg nie jest rosnący
             czyRosnacy = False
 
-        # Jeśli aktualny ciąg nie jest rosnący i obecna liczba jest większa lub równa poprzedniej, to ciąg nie jest malejący
+        # Jeśli aktualny ciąg jest malejacy i rośnie lub liczba jest taka sama jak poprzednia
         elif not czyRosnacy and czyMalejacy and obecnaLiczba >= poprzedniaLiczba:
 
             # Ciąg nie jest malejący
